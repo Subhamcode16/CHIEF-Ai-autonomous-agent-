2fd783c038722b80d5e08f60121c8a0dbd849c9a
Subham Rath <subham.rath1611@gmail.com>
2026-02-13 00:11:27 +0530

    CORS update

--- backend/server.py
+++ backend/server.py
@@ -1032,7 +1032,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=[origin.strip() for origin in os.environ.get('CORS_ORIGINS', 'http://localhost:3000').split(',') if origin.strip()],
     allow_methods=["*"],
     allow_headers=["*"],
 )
